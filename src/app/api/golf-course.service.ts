45be23fbd2c4a4d086f510815f4f753410079697
Twitch0125 <twitcherc@gmail.com>
2019-08-28 11:20:24 -0600

    getCourseById()

--- src/app/api/golf-course.service.ts
+++ src/app/api/golf-course.service.ts
@@ -1,16 +1,21 @@
+import { AjaxResponse, ajax } from 'rxjs/ajax';
+
 import { Course } from '../types/course';
 import { Courses } from '../types/courses';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ajax } from 'rxjs/ajax';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GolfCourseService {
   constructor() {}
+  API_URL = 'https://golf-courses-api.herokuapp.com';
 
-  getCourses(): Observable<any> {
-    return ajax('https://golf-courses-api.herokuapp.com/courses');
+  getCourses(): Observable<AjaxResponse> {
+    return ajax(`${this.API_URL}/courses`);
+  }
+  getCourseById(id: number): Observable<AjaxResponse> {
+    return ajax(`${this.API_URL}/courses/${id}`);
   }
 }
