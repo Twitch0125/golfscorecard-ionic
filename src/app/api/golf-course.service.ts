2d910a4fe74bd9a95f7b36d20eb0f06b07be7b81
Twitch0125 <twitcherc@gmail.com>
2019-08-27 22:44:26 -0600

    service

--- src/app/api/golf-course.service.ts
+++ src/app/api/golf-course.service.ts
@@ -1,9 +1,16 @@
+import { Course } from '../types/course';
+import { Courses } from '../types/courses';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GolfCourseService {
+  constructor() {}
 
-  constructor() { }
+  getCourses(): Observable<any> {
+    return ajax('https://golf-courses-api.herokuapp.com/courses');
+  }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GolfCourseService } from '../api/golf-course.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(private coursesService: GolfCourseService) {}
+
+  courses = this.coursesService.getCourses();
 }
