5b8d05c274cf3bddcab6a246f07b4e20571dac32
Twitch0125 <twitcherc@gmail.com>
2019-08-29 11:14:19 -0600

    added min to range

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
   players = [];
-  value: number;
+  value: 1;
   constructor() {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+  }
 
   handleIonRangeChange() {
     console.log('range change', this.value);
